05c8d36e82c32b93f86324314c0e206325f0191e
Mauricio A <maramburu98@gmail.com>
2019-11-06 14:03:12 -0600

    Added movement
    Added horizontal player movement and jump. Potential sprint to be implemented later

---
+++ Wangz Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Used to reference controller script
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        //Checks if user is trying to jump
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        /* //Sprint - not implemented correctly
+        if(Input.GetKeyDown("left shift"))
+        {
+            runSpeed = 50f;
+            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        }*/
+    }
+
+    void FixedUpdate()
+    {
+        //Moves player 
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false; //Resets jump 
+    }
+}
