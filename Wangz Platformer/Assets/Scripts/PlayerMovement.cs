1d76f24d19c03a1e2db15998f297a3714a11232a
Mauricio A <maramburu98@gmail.com>
2019-11-06 16:03:46 -0600

    Animations
    Added idle, jump, running, and crouch animations

--- Wangz Platformer/Assets/Scripts/PlayerMovement.cs
+++ Wangz Platformer/Assets/Scripts/PlayerMovement.cs
@@ -6,35 +6,52 @@ public class PlayerMovement : MonoBehaviour
 {
     //Used to reference controller script
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
     bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         //Checks if user is trying to jump
         if(Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("isJumping", true);
         }
 
-        /* //Sprint - not implemented correctly
-        if(Input.GetKeyDown("left shift"))
+        if(Input.GetButtonDown("Crouch") || Input.GetKeyDown(KeyCode.LeftControl))
         {
-            runSpeed = 50f;
-            horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        }*/
+            crouch = true;
+        }
+        else if(Input.GetButtonUp("Crouch") || Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    public void onCrouching(bool isCrouching)
+    {
+        animator.SetBool("isCrouching", isCrouching);
     }
 
     void FixedUpdate()
     {
         //Moves player 
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false; //Resets jump 
     }
 }
